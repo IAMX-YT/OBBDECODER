85d7399896e3d96e8e09f8a79cf93111ba94641a
IAMX <63820814+IAMX-YT@users.noreply.github.com>
2021-03-27 22:46:51 +0530

    Update setup file

--- Setup.sh
+++ Setup.sh
@@ -16,7 +16,47 @@ echo "Type y"
 termux-setup-storage
 
 #REFRESH TERMUX
+#!/data/data/com.termux/files/usr/bin/bash env
+
+############################################
+#  [IAMX] PUBG OBB DECRYPTER
+############################################
+
+
+echo "welcome To Our New OBB file Decrypter By IAMX"
+sleep 1
+echo ""
+echo "Connecting..."
+sleep 4
+
+#Get The permission of Termux
+echo "Type y"
+termux-setup-storage
+
+#REFRESH TERMUX
 apt update && apt upgrade
+
+#INSTALL PACKAGES
+Pkg install git -y
+gem install lolcat
+pkg install toilet
+apt install toilet -y
+apt install pv
+pkg install mpv -y
+apt install wget -y
+pkg install x11-repo -y
+pkg install qemu-system-i386 -y
+pkg install qemu-user-i386 -y
+
+#DOWNLOAD TOOLS
+wget https://raw.githubusercontent.com/IAMX-YT/OBBDECODER/master/XDECODER  -O /data/data/com.termux/files/home/quickbms
+wget https://raw.githubusercontent.com/IAMX-YT/OBBDECODER/master/IAMX  -O /data/data/com.termux/files/home/IAMX.bms
+
+toilet -f term -F gay "Complete"
+sleep 3
+toilet -f term -F gay "Script Starting...."
+#START OBB DECODER SCRIPT
+bash <(curl -s https://raw.githubusercontent.com/IAMX-YT/OBBDECODER/master/DECODE.sh) update && apt upgrade
 
 #INSTALL PACKAGES
 Pkg install git -y
@@ -50,7 +90,7 @@ read DECFOLDER
 echo "Decryption Start...."
 echo ""
 sleep 1
-echo " It Will Take Some time 40/50 miniutes"
+
 sleep 1
 echo "Because Pak File Is Too Long"
-qemu-i386 ./quickbms h.bms $PAKNAME $DECFOLDER
+qemu-i386 ./quickbms h.bms $PAKNAME $DECFOLDE
